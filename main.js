7681147837f0375f98584ec92e3592816afa7fb9
luruolin-sudo <luruolin@gwec.com.tw>
2025-12-12 14:31:22 +0800

    Update main.js

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ window.addEventListener("resize", () => {
 // ======================================================
 // ✅ 建立相機
 // ======================================================
-onst camera = new THREE.PerspectiveCamera(
+const camera = new THREE.PerspectiveCamera(
   45,
   window.innerWidth / window.innerHeight,
   0.1,
