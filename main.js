4287e031fbd3e47447ec0e1ba3717bf101891076
luruolin-sudo <luruolin@gwec.com.tw>
2025-12-12 15:32:13 +0800

    Update main.js

--- main.js
+++ main.js
@@ -45,13 +45,13 @@ const camera = new THREE.PerspectiveCamera(
 const isMobile = window.innerWidth <= 768;
 
 // ✅ 根據裝置調整鏡頭距離
-const cameraZ = isMobile ? 3.0 : 0.8;   // 手機拉遠、桌機較近
+const cameraZ = isMobile ? 3.0 : 0.9;   // 手機拉遠、桌機較近
 const cameraY = isMobile ? -0.15 : -0.15;     // 手機視角稍微往上  高度仰角
-const cameraX = isMobile ? -0.2 : -0.2;     // 手機視角更置中
+const cameraX = isMobile ? -0.25 : -0.25;     // 手機視角更置中
 
 // ✅ 套用相機位置
 camera.position.set(cameraX, cameraY, cameraZ);
-camera.lookAt(0, 0, 0);
+camera.lookAt(0, 0, 0.1);
 
 // ✅ 視角也依裝置調整
 camera.fov = isMobile ? 40 : 45;
